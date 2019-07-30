45c606e2b499db729175bde8f116b7fcf3918014
Kateryna Tkachenko <katherinatkachenko@gmail.com>
2019-07-30 16:10:56 +0300

    Fourth step

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
     <h1>TODOS</h1>
-      <input v-model="newTodo" placeholder="New todo">
-      <button v-on:click="addTodo">Add</button>
-      <ul>
-        <li v-for="(todo, index) in todos" :key='index' >
-          {{todo}}
-        </li>
-      </ul>
+    <input v-model="newTodo" placeholder="New todo">
+    <button v-on:click="addTodo">Add</button>
+    <ul v-for="(todo, index) in todos" :key='index'>
+      <li>
+        {{todo}}
+        <button v-on:click="removeTodo(index)">Remove</button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-
 export default {
   name: "App",
   data() {
@@ -22,9 +22,13 @@ export default {
     }
   },
   methods: {
-    addTodo (newTodo) {
+    addTodo () {
       this.todos.push(this.newTodo);
-      this.newTodo = ''
+      this.newTodo = '';
+    },
+    
+    removeTodo (index) {
+      this.todos.splice(index, 1);
     }
   }
 }
