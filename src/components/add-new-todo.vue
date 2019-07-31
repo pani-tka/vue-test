dc6a13e355a5143baf13542d5081b47437d8f3ff
Kateryna Tkachenko <katherinatkachenko@gmail.com>
2019-07-31 10:51:40 +0300

    Current changes and bug fixing

---
+++ src/components/add-new-todo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <input v-model="newTodo" placeholder="New todo"/>
+    <button @click='clearTodoInput'>Add</button>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.component('AddNewTodo', {
+  data() {
+    return {
+      newTodo: ''
+    }
+  },
+  methods: {
+    clearTodoInput () {
+      this.$emit('addTodo', this.newTodo);
+      this.newTodo = '';
+    },
+  }
+})
+</script>
