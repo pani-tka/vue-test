34c56504d5bc9b023869cd4389615e43cda53674
Kateryna Tkachenko <katherinatkachenko@gmail.com>
2019-07-31 19:30:27 +0300

    Created TodoList component and changed app-structure

---
+++ src/components/todo-list.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+    <li v-for="(todo, index) in todos" :key='index'>
+      {{todo}}
+      <button @click="$emit('removeTodo', index)">Remove</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.component('TodoList', {
+  props: {
+    todos: Array
+  }
+})
+
+</script>
